943538788f865c98955afa8a813bbdef8dd7741c
dani <danieljuhe@gmail.com>
2023-01-01 20:02:57 +0100

    Authentification in Yarn with Auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { LoginButton } from './login';
+import { LogOut } from './logout';
+import { Profile } from './profile'
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { isAuthenticated ? (
+          <>
+          <Profile/>
+          <LogOut/>
+          </>
+        ) : <LoginButton/>}
       </header>
     </div>
   );
